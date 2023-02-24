4add600937d9f8be7bcb939eea1f8a7c0920d70b
Ricardo Guillen I <r.arturogi@gmail.com>
2023-02-24 16:18:20 -0400

    Update resume data.

--- js/scratchpad.js
+++ js/scratchpad.js
@@ -2,104 +2,81 @@
 //--------------------------------------------------------------------------------
 const intro = [
   `
-  <!-- ::Lead Front-End Developer @ Elaniin::
+  <!-- 
+  
+  :: Resume ::
 
   {
     "name": 'Ricardo Guillen I.',
-    "jobTitle": 'Front-End Developer',
+    "job_title": 'Senior Front-End Developer',
     "motto": 'Coffee, Code, Sleep, Repeat',
     "skills": [
-      "Web_Development": {
+      "web_development": {
         "Javascript": [
-          "ES6",
-          "VueJs", 
-          "NuxtJs", 
           "ReactJs",
           "NextJs",
-          "NodeJs"
+          "VueJs",
+          "NuxtJs",
+          "NodeJs",
+          "TypeScript"
         ],
-        "Database": [
+        "databases": [
           "MongoDB",
           "MySQL",
-          "Postgresql"
+          "PostgreSQL",
+          "GraphQL"
         ]
       },
-      "Web_Design": [
+      "web_design": [
         "TailwindCSS",
-        "Javascript",
         "HTML5",
         "CSS3",
-        "Figma",
-        "Adobe XD"
+        "Figma"
       ]
     ]
   } 
-    ::Keep Scrolling Down::
--->
+    ::Keep Scrolling Down:: 
+    -->
 
-<div class="wrapper">
+ <div class="wrapper">
+    <img class="avatar" src="./assets/imgs/JPG/logo.jpg" title="Ricardo GI, Senior Front-End Developer" />
 
-<!-- Photo -->
-<img class="avatar" src="./assets/imgs/JPG/logo.jpg" title="Ricardo GI, Lead Front-End Developer" />
+    <div class="content">
+      <section class="headings">
+        <h1 class="title">Ricardo Guillen I.</h1>
+        <h2 class="subtitle">Senior Front-End Developer</h2>
+        
+        <div class="networks">
+          <a class="networks__item" href="https://linkedin.com/in/ryarturogi" title="LinkedIn" target="_blank">
+            <img class="networks__item-icon" src="./assets/imgs/SVG/linkedin-in.svg" title="LinkedIn" />
+          </a>
 
-<div class="content">
+          <a class="networks__item" href="https://github.com/ryarturogi" title="Github" target="_blank">
+            <img class="networks__item-icon" src="./assets/imgs/SVG/github.svg" title="Github" />
+          </a> 
 
-  <section class="headings">
+           <a class="networks__item" href="https://twitter.com/ryarturogi" title="Twitter" target="_blank">
+            <img class="networks__item-icon" src="./assets/imgs/SVG/twitter.svg" title="Twitter" />
+          </a>
+        </div>
+      </section>
 
-    <!-- Full Name -->
-    <h1 class="title">Ricardo Guillen I.</h1>
+      <section class="skills">
+        <hgroup>
+          <h2>
+            <strong>Stack ⚡</strong>
+          </h2>
 
-    <!-- Job Title -->
-    <h2 class="subtitle">Lead Front-End Developer at @Elaniin</h2>
-
-    <!-- Social Networks -->
-    <div class="networks">
-      <a class="networks__item" href="https://github.com/ryarturogi" title="Github" target="_blank">
-        <img class="networks__item-icon" src="./assets/imgs/SVG/github.svg" title="Github" />
-      </a>
-      <a class="networks__item" href="https://facebook.com/ryarturogi" title="Facebook" target="_blank">
-        <img class="networks__item-icon" src="./assets/imgs/SVG/facebook.svg" title="Facebook" />
-      </a>
-      <a class="networks__item" href="https://twitter.com/ryarturogi" title="Twitter" target="_blank">
-        <img class="networks__item-icon" src="./assets/imgs/SVG/twitter.svg" title="Twitter" />
-      </a>
-      <a class="networks__item" href="https://linkedin.com/in/ryarturogi" title="LinkedIn" target="_blank">
-        <img class="networks__item-icon" src="./assets/imgs/SVG/linkedin-in.svg" title="LinkedIn" />
-      </a>
+          <div>
+            Javascript/Typscript, React, Next.js, Vue, Nuxt.js, Node.js, MySQL, NoSQL, GraphQL, TailwindCSS, Figma.
+          </div>
+        </hgroup>
+      </section>
     </div>
 
-  </section>
+    <a class="contact-action" href="mailto:r.arturogi@gmail.com" title="Contact me!" target="_blank">Drop a message <span class="contact-action__icon">📩</span></a>
 
-  <!-- Skills -->
-  <section class="skills">
-    <hgroup>
-      <h2>
-        <strong>Skills ${new Date().getFullYear()}⚡</strong>
-      </h2>
-
-      <h3>
-        <b>Web Development</b>
-      </h3>
-      <p>
-        JavaScript: ES6, VueJs, NuxJs, ReactJs, NextJS, NodeJs, DB: MongoDB, MySQL, Postgresql
-      </p> <br />
-
-      <h3>
-        <b>Website Design</b>
-      </h3>
-      <p>
-        TailwindCSS, JavaScript, HTML5, CSS, Figma, Adobe XD
-      </p>
-    </hgroup>
-  </section>
-</div>
-<!-- Contact Button -->
-<a class="contact-action" href="mailto:r.arturogi@gmail.com" title="Contact me!" target="_blank">Contact Me <span
-    class="contact-action__icon">&phone;</span></a>
-
-</div>
-
-</div>
+  </div>
 
 
 <style>
@@ -116,241 +93,186 @@ const intro = [
    
   */
 
-/*
-  0.0 - Root
-  */
+/* 0.0 - Root */
 
 :root {
+  /* colors */
   --main-bg-color: #1c1c1c;
   --link-color: #99cc99;
-
-  /* colors */
   --white-color: #fff;
   --black-color: #0a0a0a;
   --silver-color: #ddd;
   --blue-color: #0085ff;
-
 }
 
-/*  
-  1.0 - Reset
-  */
+/* 1.0 - Reset */
 
-* {
+ {
   box-sizing: border-box;
   margin: 0;
   padding: 0;
 }
-
-/*  
-  2.0 - Globals
-  */
+/* 2.0 - Globals */
 
 body {
-  display: flex;
-  background-color: const(--silver-color);
-  background-color: #f2f2f2;
+  background-color: var(--white-color);
+  font-family: 'Open Sans', 'Raleway', 'Tahoma', sans-serif;
 }
 
 .wrapper {
-  color: var(--main-bg-color);
-  margin: auto;
   width: 280px;
   height: auto;
-  font-family: Open Sans, Raleway, Tahoma, sans-serif;
+  margin: auto;
+  padding: 1rem 1.5rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
   text-align: center;
-  justify-items: center;
   border-radius: 8px;
   overflow: hidden;
-  background: #f2f2f2;
-  padding: 1rem 0;
-
+  background: #fff;
+  color: var(--main-bg-color);
 }
-
-/*  
-  Avatar 
-  */
-
 
 .avatar {
-  width: 100%;
-  max-width: 100%;
-  margin: 0;
-  display: block;
-  height: 320px;
+  width: 15rem;
+  height: 15rem;
+  border-radius: 25%;
   object-fit: cover;
-  object-position: bottom;
+  margin-bottom: 1rem;
 }
 
-/*  
-  Content
-  */
-
-.content {
-  background: var(--white-color);
-  margin-top: -2.25em;
-  padding: 0 2em 1em;
-}
-
-/*  
-  Headings
-  */
-
+/* 3.0 - Headings */
 
 .headings {
-  display: block;
-  margin: 1em 0 1.5em;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+  margin: 1rem;
 }
 
 .title {
-  color: var(--black-color);
-  font-size: 1.25rem;
-  font-weight: 800;
-  margin: 0;
+  font-size: 1.75rem;
+  margin-bottom: 0.25rem;
 }
 
 .subtitle {
-  color: var(--black-color);
-  font-size: .875rem;
-  width: 340px;
-  font-weight: normal;
-  margin: 0 0 0.5em;
-  padding: 0 3em;
+  font-size: 1.25rem;
+  font-weight: 500;
+  margin-bottom: 1.5rem;
 }
 
-/*  
-  Networks 
-  */
+/* 4.0 - Networks */
 
 .networks {
   display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 1rem;
 }
 
 .networks__item {
-  margin: 0 0.35rem;
+  margin: 0 0.5rem;
 }
 
 .networks__item-icon {
-  width: 18px;
+  width: 2rem;
+  height: 2rem;
+  object-fit: contain;
 }
 
-/*  
-  Skills 
-  */
+/* 5.0 - Skills */
 
 .skills {
-  max-width: 100%;
-  display: block;
-  clear: both;
-  margin: 0;
-  padding: 0 5em;
-  font-size: .75rem;
-  color: var(--color-white);
+  margin: 1rem;
 }
 
-.skills h2 {
-  margin-bottom: .5em;
+hgroup {
+  margin-bottom: 1rem;
 }
 
+hgroup h2 {
+5rem;
+  margin-bottom: 0.75rem;
+}
 
-/*  
-  Contact
-  */
+.skills div {
+  font-size: 1rem;
+  margin-bottom: 0.75rem;
+}
 
+/* 6.0 - Contact */
 
 .contact-action {
-  color: var(--blue-color);
-  font-weight: 700;
-  font-size: 1rem;
+  background-color: var(--main-bg-color);
+  color: var(--white-color);
+  padding: 0.75rem 1rem;
+  border-radius: 10px;
   text-decoration: none;
-  text-align: center;
-  text-transform: uppercase;
-  display: block;
-  width: 100%;
-  max-width: 100%;
-  margin: 0;
-  padding: .5em .75em .75em;
-  background: var(--white-color);
-  border: 2px solid var(--blue-color);
-  border-radius: 0 0 8px 8px;
-  transition: ease 0.2s all;
-  -webkit-transition: ease 0.2s all;
-  -moz-transition: ease 0.2s all;
+  margin-bottom: 1rem;
 }
 
 .contact-action:hover {
-  background: var(--blue-color);
-  border-top: 2px solid var(--blue-color);
-  color: var(--white-color);
+  background-color: var(--link-color);
+  transition: background-color 0.2s ease-in-out;
 }
 
 .contact-action__icon {
-  position: relative;
-  top: 2px;
-  left: 2px;
-  font-size: 1.35rem;
+  margin-left: 0.5rem;
+  font-size: 0.8rem;
 }
-</style>
-  `,].join("\n");
-  
-$(function () {
 
+</style>
+  `,
+].join('\n');
+
+$(function () {
   // Ace code editor
   //--------------------------------------------------------------------------------
-  const editor = ace.edit("editor");
-  editor.setTheme("ace/theme/cobalt");
-  editor.getSession().setMode("ace/mode/html");
+  const editor = ace.edit('editor');
+  editor.setTheme('ace/theme/cobalt');
+  editor.getSession().setMode('ace/mode/html');
   editor.setHighlightActiveLine(true);
   editor.getSession().setTabSize(1);
   editor.setOptions({
-    fontFamily: "JetBrains Mono, monospace",
-    fontSize: "14px",
-    displayIndentGuides: true,
+    fontFamily: 'JetBrains Mono, monospace',
+    fontSize: '14px',
+    displayIndentGuides: false,
     showGutter: false,
     readOnly: false,
   });
-  editor.commands.removeCommand("gotoline");
+  editor.commands.removeCommand('gotoline');
   editor.setShowPrintMargin(false);
   editor.commands.addCommand({
-    name: "showHelp",
-    bindKey: { win: "Ctrl-/", mac: "Command-/" },
+    name: 'showHelp',
+    bindKey: { win: 'Ctrl-/', mac: 'Command-/' },
     exec: function () {
-      $("#help").toggleClass("visible");
+      $('#help').toggleClass('visible');
     },
   });
   editor.commands.addCommand({
-    name: "toggleFullscreen",
-    bindKey: { win: "Ctrl-i", mac: "Command-i" },
+    name: 'toggleFullscreen',
+    bindKey: { win: 'Ctrl-i', mac: 'Command-i' },
     exec: function () {
       toggleFullscreen();
     },
   });
 
   // Set up iframe.
-  const iframe = document.getElementById("preview"),
+  const iframe = document.getElementById('preview'),
     iframedoc = iframe.contentDocument || iframe.contentWindow.document;
-  iframedoc.body.setAttribute("tabindex", 0);
+  iframedoc.body.setAttribute('tabindex', 0);
 
   // Base firebase ref
   //--------------------------------------------------------------------------------
   const scratchpadRef = new Firebase(
-    "https://rys-portfolio.firebaseio.com/" + Scratchpad.document_id
+    'https://rys-portfolio.firebaseio.com/' + Scratchpad.document_id
   );
 
   // Code Editing
   //--------------------------------------------------------------------------------
-  const scratchpadEditorRef = scratchpadRef.child("editor");
+  const scratchpadEditorRef = scratchpadRef.child('editor');
 
   // When code changes, put it into the editor
-  scratchpadEditorRef.on("value", function () {
+  scratchpadEditorRef.on('value', function () {
     editor.setValue(`${intro}`);
     // Clear selection and move cursor to where it needs to be
     editor.clearSelection();
@@ -358,46 +280,46 @@ $(function () {
 
   // On keyup, save the code and cursor data to firebase
   let typingTimeout;
-  $("#editor").on("keyup", function () {
+  $('#editor').on('keyup', function () {
     // Tell firebase who is editing
     window.clearTimeout(typingTimeout);
-    thisClientRef.set("typing");
+    thisClientRef.set('typing');
 
     // Set a timeout for 2 seconds that tells firebase who is typing
     typingTimeout = setTimeout(function () {
-      thisClientRef.set("idle");
+      thisClientRef.set('idle');
     }, 2000);
   });
 
   // On data change, re-render the code in the iframe.
-  editor.getSession().on("change", function (e) {
+  editor.getSession().on('change', function (e) {
     iframedoc.body.innerHTML = editor.getValue();
     // Resize the menu icon if appropriate
     const linesOfCode = editor.session.getLength();
     if (linesOfCode < 10) {
-      $("#menu").attr("class", "small");
+      $('#menu').attr('class', 'small');
     } else if (linesOfCode > 9 && linesOfCode < 99) {
-      $("#menu").attr("class", "medium");
+      $('#menu').attr('class', 'medium');
     } else if (linesOfCode > 99 && linesOfCode < 999) {
-      $("#menu").attr("class", "large");
+      $('#menu').attr('class', 'large');
     } else if (linesOfCode > 999) {
-      $("#menu").attr("class", "x-large");
+      $('#menu').attr('class', 'x-large');
     }
   });
 
   // Show different tooltip for Windows users.
-  const isMac = navigator.platform.toUpperCase().indexOf("MAC") !== -1;
+  const isMac = navigator.platform.toUpperCase().indexOf('MAC') !== -1;
   if (!isMac) {
-    $(".tooltip").html("Keyboard Shortcut: Control + i");
+    $('.tooltip').html('Keyboard Shortcut: Control + i');
   }
 });
 
 // Show/Hide button code editor mobile
 // --------------------------------------------------------------------------------
 
-const showPreview = document.querySelector(".hide-show-toggle");
-const hidePreview = document.querySelector(".hidden");
+const showPreview = document.querySelector('.hide-show-toggle');
+const hidePreview = document.querySelector('.hidden');
 
 showPreview.onclick = function changeContent() {
-  hidePreview.classList.toggle("visible");
+  hidePreview.classList.toggle('visible');
 };
